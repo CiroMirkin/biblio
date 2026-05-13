6796981963fda607c0cc74d156abb5aaf7b39951
CiroMirkin <ciromirkin@gmail.com>
2026-05-13 16:31:46 -0300

    prototipo-maquetado

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const views = [
   {
     id: options.INSCRIPCION,
     view: <Inscripcion />,
-    bgColor: "bg-purple-100",
+    bgColor: "bg-[#cadbf0]",
   },
 ]
 
@@ -37,7 +37,7 @@ function App() {
           onClick={() => setActualView(options.CUOTA)}
           className="text-lg hover:underline py-3 pl-4 pr-6 rounded-t rounded-tr-2xl bg-secondary font-semibold"
         >Prestamo</button>
-        <button onClick={() => setActualView(options.INSCRIPCION)} className="text-lg hover:underline py-3 pl-4 pr-6 rounded-t rounded-tr-2xl bg-purple-100">Inscripción</button>
+        <button onClick={() => setActualView(options.INSCRIPCION)} className="text-lg hover:underline py-3 pl-4 pr-6 rounded-t rounded-tr-2xl bg-[#cadbf0]">Inscripción</button>
       </nav>
 
       <main className={`p-4 rounded-b rounded-r ${bg}`}>
@@ -51,7 +51,7 @@ export default App
 
 function Socios() {
   const [ lista, setLista ] = useState(false)
-  const [ cutoas, setCuotas] = useState(false)
+  const [ cuotas, setCuotas] = useState(false)
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
@@ -61,35 +61,49 @@ function Socios() {
   }
   return (
     <>
-      <h2 className="mb-2 text-xl font-semibold">Gestión de cuotas y socios</h2>
-        <form className="flex gap-2 rounded py-4 mb-4" onSubmit={handleSubmit}>
+      <div className="">
+        <h2 className="mb-2 text-xl font-semibold">Gestión de cuotas y socios</h2>
+        <main className="w-full grid grid-cols-[3.5fr_1.5fr] gap-4 pt-4">
+          <div>
+            <form className="flex gap-2 rounded pb-4 mb-2" onSubmit={handleSubmit}>
+            <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Apellido del socio" />
+            <input type="submit" value="Buscar" className="px-4 bg-[#8cbfb3] rounded" />
+          </form>
 
-        <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Apellido del socio" />
-        <input type="submit" value="Buscar" className="px-4 bg-[#fe8753] rounded" />
+          {lista && (
+            <ul className="w-full flex flex-col pb-4">
+              <li className="px-4 py-4 flex gap-2 justify-between bg-white">
+                <span className="w-full text-lg">Mirkin Ciro</span>
+                <button className="px-4 bg-[#8cbfb3] rounded" onClick={() => {
+                  setLista(false)
+                  setCuotas(true)
+                }}>Seleccionar</button>
+              </li>
+              <li className="px-4 py-4 flex gap-2 justify-between bg-white/50">
+                <span className="w-full text-lg">Mirkin Gustavo</span>
+                <button className="px-4 bg-[#8cbfb3] rounded">Seleccionar</button>
+              </li>
+              <li className="px-4 py-4 flex gap-2 justify-between bg-white">
+                <span className="w-full text-lg">Mino Felipe</span>
+                <button className="px-4 bg-[#8cbfb3] rounded">Seleccionar</button>
+              </li>
+            </ul>
+          )}
+          </div>
+          {!cuotas && <aside className="p-4 rounded bg-white text-base">
+            <p>Dentro de esta sección puedes:</p>
+            <ul className="pl-4 pt-1 ml-1 list-disc">
+              <li>Buscar socios según su apellido,</li>
+              <li>ver sus datos personales</li>
+              <li>el estado de sus cuotas,</li>
+              <li>el préstamo y devolución de libros,</li>
+              <li>darlo de baja o reincribirlo.</li>
+            </ul>
+          </aside>}
+        </main>
+      </div>
 
-        </form>
-
-      {lista && (
-      <ul className="w-8/12 flex flex-col pt-2 pb-4">
-        <li className="p-2 py-4 flex gap-2 justify-between bg-[#ffd396]">
-          <span className="w-full text-lg">Mirkin Ciro</span>
-          <button className="px-4 bg-[#fe8753] rounded" onClick={() => {
-            setLista(false)
-            setCuotas(true)
-          }}>Seleccionar</button>
-        </li>
-        <li className="p-2 py-4 flex gap-2 justify-between">
-          <span className="w-full text-lg">Mirkin Gustavo</span>
-          <button className="px-4 bg-[#fe8753] rounded">Seleccionar</button>
-        </li>
-        <li className="p-2 py-4 flex gap-2 justify-between bg-[#ffd396]">
-          <span className="w-full text-lg">Mino Felipe</span>
-          <button className="px-4 bg-[#fe8753] rounded">Seleccionar</button>
-        </li>
-      </ul>
-    )}
-
-    { cutoas && (
+    { cuotas && (
       <div className="grid grid-cols-2 gap-10">
         <div className="flex flex-col gap-8">
           <div className="bg-white rounded p-4">
@@ -104,7 +118,7 @@ function Socios() {
           </div>
 
           <div className="flex flex-col gap-4 bg-white rounded p-4">
-            <h2 className="text-xl font-semibold">Libros en Préstamo:</h2>
+            <h2 className="text-xl font-semibold">Libros en Préstamo</h2>
 
             <form className="w-full flex flex-col rounded">
               <div className="flex justify-between gap-2 rounded py-3 px-2 bg-gray-200">
@@ -112,7 +126,7 @@ function Socios() {
                     <span>Llamadas telefónicas</span> 
                     <span>N° 19116</span>
                     </div>
-                  <button className="px-4 bg-[#fe8753] rounded">Devuelto</button>
+                  <button className="px-4 bg-[#8cbfb3] rounded">Devuelto</button>
               </div>
 
               <div className="flex justify-between gap-2 rounded py-3 px-2 bg-white">
@@ -120,7 +134,7 @@ function Socios() {
                     <span>La sed</span>
                     <span>N° 18869</span>
                   </div>
-                  <button className="px-4 bg-[#fe8753] rounded">Devuelto</button>
+                  <button className="px-4 bg-[#8cbfb3] rounded">Devuelto</button>
               </div>
 
               <div className="flex gap-2 py-3 px-2 rounded bg-gray-200">
@@ -133,68 +147,73 @@ function Socios() {
                 <input type="text" name="" id="" className="border border-black bg-white rounded p-1 px-2" placeholder="N° de Inventario" />
               </div>
 
-              <button className="bg-[#fe8753] mt-4 p-1 pb-2 text-lg rounded">Registrar préstamo</button>
+              <button className="bg-[#8cbfb3] mt-4 p-1 pb-2 text-lg rounded">Registrar préstamo</button>
             </form>
-          </div>
-
-          <div className="flex gap-4 bg-white rounded p-4">
-            <button className="px-4 pb-1 rounded bg-sky-200">Dar de baja</button>
-            <button className="px-4 pb-1 rounded bg-sky-200">Re inscribir</button>
           </div>
         </div>
 
-        <div className="">
-          <h2 className="text-xl mb-4">Cuotas:</h2>
-          <ul className="grid sm:grid-cols-3 grid-cols-2 gap-2">
-            <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Enero</span>
-              <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Febrero</span>
-              <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Marzo</span>
-              <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Abril</span>
-              <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Mayo</span>
-              <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Junio</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Julio</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Agosto</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Septiembre</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Octubre</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Noviembre</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-            <li className="px-3 pb-2 pt-1 rounded bg-white flex flex-col gap-2 justify-center">
-              <span className="font-semibold text-lg text-center">Diciembre</span>
-              <button className="px-4 pb-1 bg-[#fe8753] rounded">No Pago</button>
-            </li>
-          </ul>
+        <div className="flex flex-col gap-4">
+          <div className="p-4 bg-white rounded">
+            <h2 className="pb-4 text-xl">Cuotas 2026</h2>
+            <ul className="grid sm:grid-cols-3 grid-cols-2 gap-2">
+              <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Enero</span>
+                <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Febrero</span>
+                <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Marzo</span>
+                <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Abril</span>
+                <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-green-300 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Mayo</span>
+                <button className="px-4 pb-1 bg-white opacity-50 hover:opacity-100 rounded">Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Junio</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Julio</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Agosto</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Septiembre</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Octubre</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Noviembre</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+              <li className="px-3 pb-2 pt-1 rounded bg-gray-200 flex flex-col gap-2 justify-center">
+                <span className="font-semibold text-lg text-center">Diciembre</span>
+                <button className="px-4 pb-1 bg-[#8cbfb3] rounded">No Pago</button>
+              </li>
+            </ul>
+            <footer className="w-full pt-4 flex gap-6 justify-center">
+              <button className="px-4 pt-px bg-[#8cbfb3] rounded">2025</button>
+              <button  className="px-4 pt-px bg-[#8cbfb3] rounded opacity-35">2027</button>
+            </footer>
+          </div>
+          <div className="mt-2 flex gap-4 bg-white rounded p-4">
+            <button className="px-4 pb-1 rounded bg-sky-200">Dar de baja</button>
+            <button className="px-4 pb-1 rounded bg-sky-200">Re inscribir</button>
+          </div>
         </div>
       </div>
     )}
@@ -213,10 +232,15 @@ function Inscripcion() {
   return (
     <>
       <h2 className="mb-2 text-xl font-semibold">Inscripción de un nuevo socio</h2>
-        <form className="flex gap-2 rounded py-4" onSubmit={handleSubmit}>
+        <form className="flex flex-col gap-2 rounded py-4" onSubmit={handleSubmit}>
 
-        <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Apellido del socio" />
-        <input type="submit" value="Buscar" className="px-4 bg-[#fe8753] rounded" />
+        <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="DNI" />
+        <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Apellido" />
+        <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Nombre" />
+        <input type="date" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Fecha de nacimiento" />
+        <input type="number" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Numero de celular" />
+        <input type="text" name="" id="" className="w-full border bg-white border-black rounded p-1 px-2" placeholder="Dirección" />
+        <input type="submit" value="Inscribir" className="px-4 py-2 bg-[#eef4fe] rounded" />
 
         </form>
     </>
