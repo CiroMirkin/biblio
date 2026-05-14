db1b5d8b4b983c3328bd6b3919ae2752cdebf9be
CiroMirkin <ciromirkin@gmail.com>
2026-05-13 23:10:05 -0300

    feat: actualiza el .gitignore para ignorar dist-electron y archivos excel en public

--- dist-electron/preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-//#region electron/preload.ts
-contextBridge.exposeInMainWorld("electronAPI", {
-	getSocios: () => ipcRenderer.invoke("getSocios"),
-	getCuotasSocio: (nroSocio) => ipcRenderer.invoke("getCuotasSocio", nroSocio),
-	toggleCuota: (nroSocio, mesIndex) => ipcRenderer.invoke("toggleCuota", nroSocio, mesIndex)
-});
-//#endregion
