ef84750a126eb80298ac90ae1511298a88a0c70c
CiroMirkin <ciromirkin@gmail.com>
2026-05-17 01:08:46 -0300

    refactor: expande props de form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,27 @@
 import { cn } from "@/utils"
-import { type ChangeEvent, type SyntheticEvent } from "react"
+import { type ChangeEvent, type HTMLInputTypeAttribute, type SyntheticEvent } from "react"
 
 type Props = {
   label: string
   placeholder?: string
   submitLabel?: string
+  inputType?: HTMLInputTypeAttribute | undefined 
+  defaultValue?: string | number
   onChange: (value: string) => void
   onSubmit?: () => void
   className?: string
 }
 
-export function Form({ label, placeholder, submitLabel = "Buscar", onChange, onSubmit, className }: Props) {
+export function Form({
+  label,
+  placeholder,
+  submitLabel = "Buscar",
+  onChange,
+  onSubmit,
+  className,
+  inputType = "text",
+  defaultValue,
+}: Props) {
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault()
     onSubmit?.()
@@ -25,7 +36,8 @@ export function Form({ label, placeholder, submitLabel = "Buscar", onChange, onS
       <label className="text-lg">{label}</label>
       <div className="mt-1 w-full flex gap-2">
         <input
-          type="text"
+          type={inputType}
+          value={defaultValue || ""}
           onChange={handleChange}
           className="w-full border bg-white border-black rounded p-1 px-2"
           placeholder={placeholder}
