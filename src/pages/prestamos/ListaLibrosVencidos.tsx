2b58b6617375085291f8e5686c95210e6beccbc8
CiroMirkin <ciromirkin@gmail.com>
2026-05-21 19:25:20 -0300

    refactor: mejora texto por defecto dentro de ListaLibrosVencidos

--- src/pages/prestamos/ListaLibrosVencidos.tsx
+++ src/pages/prestamos/ListaLibrosVencidos.tsx
@@ -5,7 +5,7 @@ export function ListaLibrosVencidos() {
     const { librosVencidos, librosFiltrados } = useLibrosStore()
     
     if(librosFiltrados.length !== 0) return
-    if (librosVencidos.length === 0) return <p className="p-4">No hay préstamos vencidos.</p>
+    if (librosVencidos.length === 0) return <p className="mt-4 card opacity-80">No hay libros en préstamo vencidos, pero puedes buscar información sobre un libro y el socio que lo posee.</p>
     
     return (
         <ul className="flex flex-col gap-3 py-4">
