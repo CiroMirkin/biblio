e661ab6698e553289e4fd2b33e956a6d478ad6ce
c.mirkin <ciromirkin@gmail.com>
2026-05-28 11:04:48 -0300

    refactor: getLibro migra hacia lowdb

--- electron/handlers/getLibros.ts
+++ electron/handlers/getLibros.ts
@@ -1,18 +1,7 @@
-import ExcelJS from 'exceljs'
 import type { LibroEnPrestamo } from '../libro'
-import { LIBROS_XLSX_PATH } from '../constants'
-import { rowToLibro } from '../utils/excelhelpers'
+import { db } from '../libros-db'
 
 export const getLibros = async (): Promise<LibroEnPrestamo[]> => {
-    const workbook = new ExcelJS.Workbook()
-    await workbook.xlsx.readFile(LIBROS_XLSX_PATH)
-    const worksheet = workbook.getWorksheet('Hoja1')
-    if (!worksheet) return []
-
-    const libros: LibroEnPrestamo[] = []
-    worksheet.eachRow((row, rowIndex) => {
-        if (rowIndex === 1) return
-        libros.push(rowToLibro(row))
-    })
-    return libros
+    await db.read()
+    return db.data
 }
