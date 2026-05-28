58f811042b1cadac951db78d72c2a64babb2ce83
c.mirkin <ciromirkin@gmail.com>
2026-05-28 11:06:53 -0300

    refactor: migra getLibrosPrestadosSocio hacia lowdb

--- electron/handlers/getLibrosPrestadosSocio.ts
+++ electron/handlers/getLibrosPrestadosSocio.ts
@@ -1,24 +1,9 @@
-import ExcelJS from 'exceljs'
-import { LIBROS_XLSX_PATH } from '../constants'
-import type { Libro } from '../libro'
-import { rowToLibro } from '../utils/excelhelpers'
+import type { LibroEnPrestamo } from '../libro'
+import { db } from '../libros-db'
 
-export const getLibrosPrestadosSocio =  async (nombreSocio: string, nroSocio?: number) => {
-  const workbook = new ExcelJS.Workbook()
-  await workbook.xlsx.readFile(LIBROS_XLSX_PATH)
-  const worksheet = workbook.getWorksheet('Hoja1')
-  if (!worksheet) return []
-
-  const libros: Libro[] = []
-
-  worksheet.eachRow((row, rowIndex) => {
-    if (rowIndex === 1) return
-
-    const libro = rowToLibro(row)
-    if (libro.numeroSocio === nroSocio || libro.nombreSocio === nombreSocio) {
-      libros.push(libro)
-    }
-  })
-
-  return libros
+export const getLibrosPrestadosSocio = async (nombreSocio: string, nroSocio?: number): Promise<LibroEnPrestamo[]> => {
+  await db.read()
+  return db.data.filter(
+    (libro) => libro.numeroSocio === nroSocio || libro.nombreSocio === nombreSocio
+  )
 }
