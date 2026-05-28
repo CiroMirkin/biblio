7b3dd48585f79a79a5b4feffc3960db2948febbc
c.mirkin <ciromirkin@gmail.com>
2026-05-28 11:09:57 -0300

    refactor: migra addLibroPrestado hacia lowdb

--- electron/handlers/addLibroPrestado.ts
+++ electron/handlers/addLibroPrestado.ts
@@ -1,43 +1,29 @@
-import ExcelJS from 'exceljs'
 import type { Libro, LibroEnPrestamo } from '../libro'
-import { LIBROS_XLSX_PATH } from '../constants'
+import { db } from '../libros-db'
 
+/** Registra el prestamo de un libro. */
 export async function addLibroPrestado(libro: Libro): Promise<LibroEnPrestamo | null> {
-  const workbook = new ExcelJS.Workbook()
-  await workbook.xlsx.readFile(LIBROS_XLSX_PATH)
-  const worksheet = workbook.getWorksheet('Hoja1')
-  if (!worksheet) return null
+  await db.read()
 
-  let targetRow: ExcelJS.Row | null = null
   const date = new Date()
+  const index = db.data.findIndex((l) => l.numeroInventario === libro.numeroInventario)
 
-  worksheet.eachRow((row, rowIndex) => {
-    if (rowIndex === 1) return
-    if (Number(row.getCell(3).value) === libro.numeroInventario) {
-      targetRow = row
+  if (index !== -1) {
+    db.data[index] = {
+      ...libro,
+      numeroSocio: libro.numeroSocio ?? null,
+      fechaDePrestamo: date,
     }
-  })
-
-  if (targetRow) {
-    (targetRow as ExcelJS.Row).getCell(1).value = libro.autor;
-    (targetRow as ExcelJS.Row).getCell(2).value = libro.titulo;
-    (targetRow as ExcelJS.Row).getCell(4).value = libro.nombreSocio;
-    (targetRow as ExcelJS.Row).getCell(5).value = libro.numeroSocio ?? null;
-    (targetRow as ExcelJS.Row).getCell(6).value = date;
-    (targetRow as ExcelJS.Row).commit()
-  } else {
-    const newRow = worksheet.addRow([
-      libro.autor,
-      libro.titulo,
-      libro.numeroInventario,
-      libro.nombreSocio,
-      libro.numeroSocio ?? null,
-      date,
-    ])
-    newRow.commit()
+  }
+  else {
+    db.data.push({
+      ...libro,
+      numeroSocio: libro.numeroSocio ?? null,
+      fechaDePrestamo: date,
+    })
   }
 
-  await workbook.xlsx.writeFile(LIBROS_XLSX_PATH)
+  await db.write()
 
   return {
     ...libro,
