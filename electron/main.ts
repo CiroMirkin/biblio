f8fc6e068c8d2507b7a4b68659034b5a46683fe6
CiroMirkin <ciromirkin@gmail.com>
2026-06-02 12:17:53 -0300

    feat: agrega icon

--- electron/main.ts
+++ electron/main.ts
@@ -45,6 +45,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'public/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
