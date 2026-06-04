a9b756d12b7ead4ab19d1875b3e030419675ff9c
CiroMirkin <ciromirkin@gmail.com>
2026-06-04 02:05:04 -0300

    fix: error al establacer el id de libros sin inventariar

--- src/pages/socios/DetallesSocio/Prestamos.tsx
+++ src/pages/socios/DetallesSocio/Prestamos.tsx
@@ -122,7 +122,7 @@ export function Prestamos() {
         titulo: formatTitulo(input.titulo),
         nombreSocio,
         numeroSocio: nroSocio ?? null,
-        numeroInventario: Number(input.numeroInventario) || 0,
+        numeroInventario: input.numeroInventario,
       }
 
       const fecha = input.fechaDePrestamo
@@ -168,7 +168,7 @@ export function Prestamos() {
           className={`flex items-center gap-2 rounded py-3 px-2 ${index % 2 === 0 ? "bg-white-accent" : "bg-white"}`}
         >
           <span className={cn("text-lg", colNro)}>
-            {libro.numeroInventario!.toString().startsWith('SN-') ? 'S/N' : libro.numeroInventario}
+            {libro.numeroInventario!.toString().startsWith('SN-') || !libro.numeroInventario ? 'S/N' : libro.numeroInventario}
           </span>
           <span className={cn("text-lg wrap-break-word", colTitulo)}>{libro.titulo}</span>
           <span className={cn("text-lg wrap-break-word", colAutor)}>{libro.autor}</span>
@@ -177,7 +177,7 @@ export function Prestamos() {
             <button
               type="button"
               className="btn btn-icon"
-              onClick={() => handleDevolver(libro.numeroInventario || 0)}
+              onClick={() => handleDevolver(libro.numeroInventario || "")}
             >
               <CheckIcon className="w-5" />
             </button>
