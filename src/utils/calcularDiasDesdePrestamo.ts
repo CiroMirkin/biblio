03cdd0040b0c20245b03df14c803f70c05a56733
CiroMirkin <ciromirkin@gmail.com>
2026-06-05 22:36:03 -0300

    refactor: mejora el calculo de dias de un prestamo

--- src/utils/calcularDiasDesdePrestamo.ts
+++ src/utils/calcularDiasDesdePrestamo.ts
@@ -1,6 +1,7 @@
 
 export function calcularDiasDesdePrestamo(fecha: Date): number {
     const hoy = new Date()
-    const diff = hoy.getTime() - fecha.getTime()
-    return Math.floor(diff / (1000 * 60 * 60 * 24))
+    const inicio = Date.UTC(fecha.getFullYear(), fecha.getMonth(), fecha.getDate())
+    const fin = Date.UTC(hoy.getFullYear(), hoy.getMonth(), hoy.getDate())
+    return Math.floor((fin - inicio) / (1000 * 60 * 60 * 24))
 }
