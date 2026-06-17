e094ca5e384c4e0d740eec4efa80a74758b6b93b
CiroMirkin <ciromirkin@gmail.com>
2026-06-16 23:47:36 -0300

    fix: bug en el recuendo de dias de un prestamo

--- src/utils/calcularDiasDesdePrestamo.ts
+++ src/utils/calcularDiasDesdePrestamo.ts
@@ -1,7 +1,18 @@
 
 export function calcularDiasDesdePrestamo(fecha: Date): number {
+    let inicio = new Date(fecha)
+
+    if (inicio.getFullYear() < 100) {
+        inicio.setFullYear(2000 + inicio.getFullYear())
+    }
+    else if (inicio.getFullYear() === 1926) {
+        // JS puede asumir que "26" es 1926
+        inicio.setFullYear(2026)
+    }
+
     const hoy = new Date()
-    const inicio = Date.UTC(fecha.getFullYear(), fecha.getMonth(), fecha.getDate())
-    const fin = Date.UTC(hoy.getFullYear(), hoy.getMonth(), hoy.getDate())
-    return Math.floor((fin - inicio) / (1000 * 60 * 60 * 24))
+    hoy.setHours(0, 0, 0, 0)
+    inicio.setHours(0, 0, 0, 0)
+    const diferenciaMilisegundos = hoy.getTime() - inicio.getTime()
+    return Math.floor(diferenciaMilisegundos / (1000 * 60 * 60 * 24))
 }
